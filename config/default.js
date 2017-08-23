132a9246c7baaa42b33200f257b78044a7ab16fb
Tim Nolet <tim@unumotors.com>
2017-08-23 21:20:00 +0200

    change text and colors

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ config.bind = '0.0.0.0'
 config.port = 3000
 
 config.version = require('../package').version
-config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'cornflowerblue'
-config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'This looks great!'
+config.backgroundColour = process.env.SIMPLE_SERVICE_BGCOLOR || 'darkseagreen'
+config.heroText = process.env.SIMPLE_SERVICE_HERO_TEXT || 'But this is even better!'
 
 module.exports = config
